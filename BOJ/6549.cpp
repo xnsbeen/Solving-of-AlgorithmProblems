7b36eacf9a2c9af87eee7c2fc0a5ef0ddd17e5d7
xnsbeen <ahohoiey@naver.com>
2019-08-03 11:01:20 +0000

    wrong answer 2261

---
+++ BOJ/6549.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <string>
+#include <map>
+#include <queue>
+#include <deque>
+#include <math.h>
+#include <sstream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	cout<<"Q";
+	return 0;
+}
